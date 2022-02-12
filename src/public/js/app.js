7a364b26b8d6d19bfe13986d4bf19d904aec65fb
JS <gjwotjd112@naver.com>
2022-02-12 16:28:12 +0900

    Make Zoom Video Chat with SOCKET IO

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,71 +16,81 @@ let roomName;
 let myPeerConnection;
 
 async function getCameras() {
-    try{
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const cameras = devices.filter((device) => device.kind === "videoinput");
-        const currentCamera = myStream.getVideoTracks()[0];
-        cameras.forEach((camera) => {
-            const option = document.createElement("option");
-            option.value = camera.deviceId;
-            option.innerText = camera.lable;
-            if(currentCamera.lable === camera.lable) {
-                option.selected = true;
-            }
-            camerasSelect.appendChild(option);
-        });
-    }catch (e) {
-        console.log(e);
-    }
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.lable;
+      if (currentCamera.lable === camera.lable) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function getMedia(deviceId){
-    const initialConstrains = {
-        audio: true,
-        video: { facingMode: "user"},
-    };
-    const cameraConstrains = {
-        audio: true,
-        video: {deviceId: { exact: deviceId } },
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
     }
-    try {
-        myStream = await navigator.mediaDevices.getUserMedia(
-            deviceId? cameraConstrains : initialConstrains
-        );
-        myFace.srcObject = myStream;
-        if (!deviceId){
-            await getCameras();
-        }
-    } catch (e) {
-        console.log(e);
-    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
 
-function handleMuteClick(){
-    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
-    if(!muted){
-        muteBtn.innerText = "Unmute";
-        muted = true;
-    } else {
-        muteBtn.innerText = "Mute";
-        muted =false;
-    }
-};
-
-function handleCameraClick(){
-    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
-    if(cameraOff){
-        cameraBtn.innerText = "Turn Camera Off";
-        cameraOff = false;
-    } else {
-        cameraBtn.innerText = "Turn Camera On";
-        cameraOff = true;
-    }
-};
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
 
 async function handleCameraChange() {
-    await getMedia(camerasSelect.value);
+  await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -93,19 +103,19 @@ const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
 async function initCall() {
-    welcome.hidden = true;
-    call.hidden = false;
-    await getMedia();
-    makeConnection();
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
 }
 
 async function hanleWelcomeSubmit(event) {
-    event.preventDefault();
-    const input = welcomeForm.querySelector("input");
-    await initCall();
-    socket.emit("join_room", input.value, );
-    roomName = input.value;
-    input.value = "";
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
 }
 
 welcomeForm.addEventListener("submit", hanleWelcomeSubmit);
@@ -113,45 +123,46 @@ welcomeForm.addEventListener("submit", hanleWelcomeSubmit);
 // Socket Code
 
 socket.on("welcome", async () => {
-    const offer = await myPeerConnection.createOffer();
-    myPeerConnection.setLocalDescription(offer);
-    console.log("send offer");
-    socket.emit("offer", offer, roomName);    
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("send offer");
+  socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer", async(offer) => {
-    myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomName);
+socket.on("offer", async (offer) => {
+  console.log("receive offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
 });
 
 socket.on("answer", (answer) => {
-    myPeerConnection.setRemoteDescription(answer);
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 socket.on("ice", (ice) => {
-    console.log("receive candidate");
-    myPeerConnection.addIceCandidate(ice);
+  console.log("receive candidate");
+  myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC Code
 
 function makeConnection() {
-    myPeerConnection = new RTCPeerConnection();
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    myPeerConnection.addEventListener("addstream", handleAddStream);
-    myStream
-        .getTracks()
-        .forEach((track) => myPeerConnection.addTrack(track, myStream));
-};
-
-function handleIce(data){
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomName);
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
-function handleAddStream(data){
-    const peerFace = document.getElementById("peerFace");
-    peerFace.srcObject = data.stream;
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
 }
